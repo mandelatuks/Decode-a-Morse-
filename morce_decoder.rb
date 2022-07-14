76f1a5b66904edbb764cf1c739e585c21a18575a
haroldmud <haroldmudosa@gmail.com>
2022-07-14 10:37:12 +0200

    word and message

--- morce_decoder.rb
+++ morce_decoder.rb
@@ -30,3 +30,15 @@ MORSE_CODE = {
 def decode_char(char)
   MORSE_CODE[char]
 end
+
+def decode_word(word)
+  new_word = word.split(/ /)
+  result = new_word.map { |char| decode_char(char) }
+  result.join
+end
+
+def decode_message(message)
+  new_message = message.split(/  /)
+  result = new_message.map { |m| decode_word(m) }
+  result.join(' ')
+end
